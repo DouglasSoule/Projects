941a13435a7c1161adcb45a612c737ddf1a253ac
Owner <owner@Douglass-MacBook-Air-2.local>
2018-10-07 23:14:55 -0400

    I have documented the process so that readers can replicate my work.

--- octhomework.py
+++ octhomework.py
@@ -22,8 +22,7 @@ def get_geolocation_data(address):
         if response.status_code == 200:
             # print(response.json())
 
-            # addresses are self-reported and human-entered; they will always be
-            # a little messy and the API will always miss a few
+
             try:
                 # print(response.json()['results'])
                 # print(response.json()['results'][0])
@@ -31,8 +30,7 @@ def get_geolocation_data(address):
                 address_components = response.json()['results'][0]['address_components']
 
 
-                # that can move index positions. I feel dirty writing a loop here.
-                # But Google made me.
+
                 for line in address_components:
                     if line['types'][0] == 'administrative_area_level_2':
                         county = line['long_name']
@@ -62,7 +60,7 @@ def get_geolocation_data(address):
 def write_output_sentence(geolocator):
     # print(geolocator)
     print(f'Some stuff about where I do journalism at my favorite place in the world, the DA newsroom: I do journalistic things for the student newspaper at {geolocator["clean_address"][:-5]}, at latitude {geolocator["latitude"]} and longitude {geolocator["longitude"]}, in {geolocator["county"]}, {geolocator["state_long"]}, {geolocator["country"]}.')
-
+#This output sentence should connect to the address I am requesting to see, which in this case is the address of the DA.
 
 def geolocate(address):
     geolocator_data = get_geolocation_data(address)
@@ -76,13 +74,13 @@ if __name__ == '__main__':
     address = '284 Prospect St., Morgantown, WV'
     # address = '1284 Prospect St., Morgantown, WV'
     geolocate(address)
-
+#This is the address of the DA, my favorite place in the world.
 
 def ping_api(address):
     p = {'address': address, 'key': geojson_key}
     # print(requests.get(geojson_stub, params=p))
     return requests.get(geojson_stub, params=p)
-
+#I want the address information of the MIC as well, so I repeat the process that I used to find the longitude and latitude for the DA. All I have to change once I rewrite this is the address itself and the output sentence.)
 
 def get_geolocation_data(address):
         county = ''
@@ -94,16 +92,14 @@ def get_geolocation_data(address):
         if response.status_code == 200:
             # print(response.json())
 
-        
+
             try:
                 # print(response.json()['results'])
                 # print(response.json()['results'][0])
                 # print(response.json()['results'][0]['address_components'])
                 address_components = response.json()['results'][0]['address_components']
 
-                # the API is maddeningly stingy with lookups -- especially for data
-                # that can move index positions. I feel dirty writing a loop here.
-                # But Google made me.
+
                 for line in address_components:
                     if line['types'][0] == 'administrative_area_level_2':
                         county = line['long_name']
@@ -132,8 +128,8 @@ def get_geolocation_data(address):
 
 def write_output_sentence(geolocator):
     # print(geolocator)
-    print(f'Some stuff about where I do journalism for school: I do journalistic things at {geolocator["clean_address"][:-5]}, at latitude {geolocator["latitude"]} and longitude {geolocator["longitude"]}, in {geolocator["county"]}, {geolocator["state_long"]}, {geolocator["country"]}.')
-
+    print(f'Some stuff about where I do journalism for school: The address information for the MIC is {geolocator["clean_address"][:-5]}, at latitude {geolocator["latitude"]} and longitude {geolocator["longitude"]}, in {geolocator["county"]}, {geolocator["state_long"]}, {geolocator["country"]}.')
+#Here I'm changing the output sentence so it is different than the one that was for the DA, my favorite place in the world.
 
 def geolocate(address):
     geolocator_data = get_geolocation_data(address)
@@ -147,3 +143,4 @@ if __name__ == '__main__':
     address = '62 Morrill Way, Morgantown, WV 26506'
     # address = '62 Morrill Way, Morgantown, WV 26506'
     geolocate(address)
+    #Since I want this address to be different from the one that is listed for the DA, my favorite place in the world, I simply change it to the address for MIC, which is the location I'm seeking locational information for. 
